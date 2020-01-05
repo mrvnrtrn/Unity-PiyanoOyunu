c5437c055b1b86998d68d67b1a0e8c441837cd62
mrvnrtrn <mervenur_turan@hotmail.com>
2020-01-05 12:28:17 +0300

    unity

---
+++ EngelsizPiyano/Assets/ses.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ses : MonoBehaviour
+{
+    public void OnPlay() {
+        GetComponent<AudioSource>().pitch=1f;
+        
+        GetComponent<AudioSource>().Play();
+       
+
+
+    }
+   
+}
---
+++ EngelsizPiyano/Assets/tiklama.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class tiklama : MonoBehaviour
+{
+    // Start is called before the first frame updateC:\Users\Merve&Buse\Documents\gamejam\Assets\
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+         
+        
+    }
+   
+    
+}
